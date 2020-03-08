9de87ad4c47074836c631c3b6ecfd6aa37da0f19
Denneth Dacara <denneth@gmail.com>
2020-03-08 20:59:13 +0800

    fixes

--- application_/config/routes.php
+++ application_/config/routes.php
@@ -8,6 +8,7 @@ $route['translate_uri_dashes'] = FALSE;
 // posts
 $route['posts'] = 'posts/index';
 $route['posts/create'] = 'posts/create';
+$route['posts/edit/$1'] = 'posts/edit/$1'; // no need to declare
 $route['posts/update'] = 'posts/update';
 $route['posts/(:any)'] = 'posts/view/$1';
 
